403639835cae8eabdcb770bc22a366d410d54472
zexa-max <tera.yaar.hu.mai.000@gmail.com>
2025-06-01 15:07:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -84,7 +84,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "image.jpge"
+    "START_IMG_URL", "image.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://graph.org//file/389a372e8ae039320ca6c.png"
